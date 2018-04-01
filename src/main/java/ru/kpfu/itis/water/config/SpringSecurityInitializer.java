97c7ee5b5148637094973738c0f928e3b0d483ba
Melnikov Semen <sema.melnikov@gmail.com>
2018-04-01 23:27:46 +0300

    R19-R21 Fixed Security - enabled by AbstractSecurityWebApplicationInitializer implementation.

---
+++ src/main/java/ru/kpfu/itis/water/config/SpringSecurityInitializer.java
@@ -0,0 +1,11 @@
+package ru.kpfu.itis.water.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * Created by Melnikov Semen
+ * 11-601 ITIS KPFU
+ */
+public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer{
+
+}
